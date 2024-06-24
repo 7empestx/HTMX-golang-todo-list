ac66704437a15bacb9fe156148f98c972bb416e9
7empestx <grant.starkman@gmail.com>
2024-06-23 22:25:21 -0600

    revert eb config changes2

--- GoApp/.platform/hooks/prebuild/build.sh
+++ GoApp/.platform/hooks/prebuild/build.sh
@@ -8,4 +8,4 @@ echo "Current directory structure:"
 ls -R
 
 echo "Building Go application..."
-TEMPL_EXPERIMENT=rawgo templ generate && go build -o bin/application ./cmd/server/main.go
+go build -o bin/application ./cmd/server/main.go
